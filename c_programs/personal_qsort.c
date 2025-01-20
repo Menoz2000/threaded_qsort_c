43116e0c6cdb7dd08bc862000e8f3c8373ac30a3
Andrea Menozzi <menozzi2000@gmail.com>
2025-01-20 16:41:02 +0100

    processo di inserimento del treshold per il sotto array

--- c_programs/personal_qsort.c
+++ c_programs/personal_qsort.c
@@ -5,6 +5,26 @@
 #include <errno.h>          // Global variables and macro to manage errors in std libraries
 #include <stdbool.h>
 
+typedef struct {
+    void *array;
+    long int left;
+    long int right;
+    size_t width;
+    int (*compar)(const void *, const void *);
+    long int slice_thresh;
+} sort_args_t;
+
+void threaded_quicksort(void *array, long int n, size_t width, int (*compar)(const void *, const void *), long int slice_tresh) {
+    sort_args_t *args = malloc(sizeof(sort_args_t));
+    if (!args) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    *args = (sort_args_t){array, 0, n - 1, width, compar, slice_tresh};
+    //quicksort(args);
+}
+
 long int get_safe_long() {
     char buffer[100];       // Input Buffer
     char *endptr;           // Pointer to find residues inside the string
@@ -30,8 +50,6 @@ long int get_safe_long() {
 
     return value;
 }
-
-void threaded_quicksort(void *array, long int n, size_t width, int (*compar)(const void *, const void *)) {}
 
 int compare(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
@@ -83,20 +101,30 @@ int main(){
     long int range_elem_array;      // Max number value in the array
     long int *arr;                  //Array to order
     long int *arr_cpy;              //Array copy for checks
+    long int slice_tresh;           //dimensione minima del sottoarray TODO
     
 
     srand(time(NULL));              //Initialize the random number generator
 
     printf("Insert the number of elements to have inside the array: ");
     n = get_safe_long();
-    if (n == -1){
-        printf("Error: Non valid input\n");
+    while (n < 1){
+        printf("Error: Non valid input\nRetry: ");
+        n = get_safe_long();
     }
 
     printf("Insert the maximum number value to insert in the array: ");
     range_elem_array = get_safe_long();
-    if (range_elem_array == -1){
-        printf("Error: Non valid input\n");
+    while (range_elem_array < 1){
+        printf("Error: Non valid input\nRetry: ");
+        range_elem_array = get_safe_long();
+    }
+
+    printf("Insert the minimum sotto-array size: ");
+    slice_tresh = get_safe_long();
+    while (slice_tresh < 1){
+        printf("Error: Non valid input\nRetry: ");
+        slice_tresh = get_safe_long();
     }
 
     arr = (long int *)malloc(n * sizeof(long int));
@@ -120,9 +148,7 @@ int main(){
 
     printf("\n");
 
-    threaded_quicksort(arr, n, sizeof(long int), compare);
-
-    qsort(arr, n, sizeof(long int), compare); //to remove after threaded_quicksort implementation
+    threaded_quicksort(arr, n, sizeof(long int), compare, slice_tresh);
 
     // Print the sorted array
     for (long int i = 0; i < n; i++) {
